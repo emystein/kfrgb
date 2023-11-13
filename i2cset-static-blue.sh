0e28eda5ac0cf9bc15fa5b8f283ce2295461d432
Emiliano Menéndez <emystein@gmail.com>
2023-11-13 19:52:08 -0300

    Set sleep 0.0080 in i2cset-static-blue.sh

--- i2cset-static-blue.sh
+++ i2cset-static-blue.sh
@@ -1,42 +1,43 @@
+sleep_duration=0.0080
+
 # Setting mode static for RAM on slot 2 in SMBus 2
 # Initializing...
 i2cset -y 2 0x61 0x08 0x53
-sleep 0.0015
+sleep $sleep_duration
 # Setting mode...
 i2cset -y 2 0x61 0x09 0x00
-sleep 0.0015
+sleep $sleep_duration
 # Setting allcolor...
 i2cset -y 2 0x61 0x31 0x00
-sleep 0.0015
+sleep $sleep_duration
 i2cset -y 2 0x61 0x32 0x00
-sleep 0.0015
+sleep $sleep_duration
 i2cset -y 2 0x61 0x33 0xff
-sleep 0.0015
+sleep $sleep_duration
 # Setting brightness...
 i2cset -y 2 0x61 0x20 0x05
-sleep 0.0015
+sleep $sleep_duration
 # Finalizing...
 i2cset -y 2 0x61 0x08 0x44
-sleep 0.0015
+sleep $sleep_duration
 
 # Setting mode static for RAM on slot 4 in SMBus 2
 # Initializing...
 i2cset -y 2 0x63 0x08 0x53
-sleep 0.0015
+sleep $sleep_duration
 # Setting mode...
 i2cset -y 2 0x63 0x09 0x00
-sleep 0.0015
+sleep $sleep_duration
 # Setting allcolor...
 i2cset -y 2 0x63 0x31 0x00
-sleep 0.0015
+sleep $sleep_duration
 i2cset -y 2 0x63 0x32 0x00
-sleep 0.0015
+sleep $sleep_duration
 i2cset -y 2 0x63 0x33 0xff
-sleep 0.0015
+sleep $sleep_duration
 # Setting brightness...
 i2cset -y 2 0x63 0x20 0x05
-sleep 0.0015
+sleep $sleep_duration
 # Finalizing...
 i2cset -y 2 0x63 0x08 0x44
-
 
