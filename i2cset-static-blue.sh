fe19dc82d6b780ebddb8db2aaf8bd52e7a798411
Emiliano Menéndez <emystein@gmail.com>
2023-11-13 19:15:12 -0300

    Fix name of ic2set-static-blue.sh

--- i2cset-static-blue.sh
+++ i2cset-static-blue.sh
@@ -1,41 +1,41 @@
 # Setting mode static for RAM on slot 2 in SMBus 2
 # Initializing...
 sudo i2cset -y 2 0x61 0x08 0x53
-sleep 0.0015
+sleep 0.0010
 # Setting mode...
 sudo i2cset -y 2 0x61 0x09 0x00
-sleep 0.0015
+sleep 0.0010
 # Setting allcolor...
 sudo i2cset -y 2 0x61 0x31 0x00
-sleep 0.0015
+sleep 0.0010
 sudo i2cset -y 2 0x61 0x32 0x00
-sleep 0.0015
+sleep 0.0010
 sudo i2cset -y 2 0x61 0x33 0xff
-sleep 0.0015
+sleep 0.0010
 # Setting brightness...
 sudo i2cset -y 2 0x61 0x20 0x05
-sleep 0.0015
+sleep 0.0010
 # Finalizing...
 sudo i2cset -y 2 0x61 0x08 0x44
-sleep 0.0015
+sleep 0.0010
 
 # Setting mode static for RAM on slot 4 in SMBus 2
 # Initializing...
 sudo i2cset -y 2 0x63 0x08 0x53
-sleep 0.0015
+sleep 0.0010
 # Setting mode...
 sudo i2cset -y 2 0x63 0x09 0x00
-sleep 0.0015
+sleep 0.0010
 # Setting allcolor...
 sudo i2cset -y 2 0x63 0x31 0x00
-sleep 0.0015
+sleep 0.0010
 sudo i2cset -y 2 0x63 0x32 0x00
-sleep 0.0015
+sleep 0.0010
 sudo i2cset -y 2 0x63 0x33 0xff
-sleep 0.0015
+sleep 0.0010
 # Setting brightness...
 sudo i2cset -y 2 0x63 0x20 0x05
-sleep 0.0015
+sleep 0.0010
 # Finalizing...
 sudo i2cset -y 2 0x63 0x08 0x44
 
