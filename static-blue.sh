8d1239386b3f4da923cb4a0e0989cc4fc1dd033a
Emiliano Menéndez <emystein@gmail.com>
2023-11-13 19:06:44 -0300

    i2cset-static-blue.sh

--- static-blue.sh
+++ static-blue.sh
@@ -1,2 +1,2 @@
-sudo ./kfrgb.sh --skip-model-detection --nowarn --smbus 2 --ramslots 2,4 --mode static --color 0,0,255 --brightness 5
+sudo ./kfrgb.sh --skip-model-detection --nowarn --smbus 2 --ramslots 2,4 --wait 0 --mode static --color 0,0,255 --brightness 5
 
--- turnoff.sh
+++ turnoff.sh
@@ -1,2 +1,2 @@
-sudo ./kfrgb.sh --skip-model-detection --nowarn --smbus 2 --ramslots 2,4 --off
+sudo ./kfrgb.sh --skip-model-detection --nowarn --smbus 2 --ramslots 2,4 --wait 0 --off
 
